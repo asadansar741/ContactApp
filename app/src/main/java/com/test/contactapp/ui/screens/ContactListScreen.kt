9d7dabfba416853919c5273b23c476e7dd0952bf
Asad Ansari <asadansar741@gmail.com>
2023-07-18 14:26:58 +0530

    Paging data success

--- app/src/main/java/com/test/contactapp/ui/screens/ContactListScreen.kt
+++ app/src/main/java/com/test/contactapp/ui/screens/ContactListScreen.kt
@@ -1,7 +1,11 @@
 package com.test.contactapp.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -9,11 +13,17 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.test.contactapp.data.model.Data
 import com.test.contactapp.ui.MainViewModel
 import com.test.contactapp.ui.theme.AppColor
 import com.test.contactapp.ui.theme.ContactAppTheme
+import androidx.paging.compose.items
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun ContactListScreen(mainViewModel: MainViewModel){
@@ -35,7 +45,7 @@ fun ContactListScreen(mainViewModel: MainViewModel){
 @Composable
 fun ContactListScreenUI(viewModel: MainViewModel){
 
-    val contactList = viewModel.getContactList().collectAsLazyPagingItems()
+    val contactList: LazyPagingItems<Data> = viewModel.getContactList().collectAsLazyPagingItems()
 
     Column(
         modifier = Modifier
@@ -44,7 +54,59 @@ fun ContactListScreenUI(viewModel: MainViewModel){
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Center
     ) {
+        LazyColumn {
+            items(items = contactList) { contact ->
+                EachRow(contact)
+            }
 
+            when (val state = contactList.loadState.refresh) { //FIRST LOAD
+                is LoadState.Error -> {
+                    //TODO Error Item
+                    //state.error to get error message
+                }
+                is LoadState.Loading -> { // Loading UI
+                    item {
+                        Column(
+                            modifier = Modifier
+                                .fillParentMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                        ) {
+                            Text(
+                                modifier = Modifier
+                                    .padding(8.dp),
+                                text = "Refresh Loading"
+                            )
+
+                            CircularProgressIndicator(color = Color.Black)
+                        }
+                    }
+                }
+                else -> {}
+            }
+
+            when (val state = contactList.loadState.append) { // Pagination
+                is LoadState.Error -> {
+                    //TODO Pagination Error Item
+                    //state.error to get error message
+                }
+                is LoadState.Loading -> { // Pagination Loading UI
+                    item {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                        ) {
+                            Text(text = "Pagination Loading")
+
+                            CircularProgressIndicator(color = Color.Black)
+                        }
+                    }
+                }
+                else -> {}
+            }
+        }
     }
 
     Box (modifier = Modifier
@@ -59,4 +121,94 @@ fun ContactListScreenUI(viewModel: MainViewModel){
             Icon(Icons.Default.Add, contentDescription = null)
         }
     }
+}
+
+
+@Composable
+fun EachRow(item: Data?) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
+        backgroundColor = Color.White
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            ProfileCard(item)
+        }
+
+    }
+}
+
+
+@Composable
+fun ProfileCard(item: Data? ) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top)
+            .clickable {  },
+        backgroundColor = Color.White
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            item?.apply {
+                ProfilePicture(
+                    avatar
+                )
+                ProfileContent(
+                    firstName = first_name ,
+                    lastName = last_name
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ProfilePicture(imageUrl:String) {
+    Card(
+        shape = RoundedCornerShape(2.dp),
+        modifier = Modifier
+            .padding(5.dp)
+            .width(100.dp)
+            .height(150.dp)
+    )
+    {
+        Image(
+            painter = rememberAsyncImagePainter(imageUrl),
+            contentDescription = null,
+            modifier = Modifier.size(128.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileContent(firstName:String, lastName:String ) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = firstName,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+        CompositionLocalProvider(LocalContentAlpha provides (ContentAlpha.medium)) {
+            Text(
+                text = lastName,
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+
 }
